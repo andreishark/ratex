40379da6c151146d1c686b4b5ff26cc1661d5665
andreishark <andreishark10@gmail.com>
2023-02-06 09:15:47 +0200

    Structuring code

--- src/json_parser/mod.rs
+++ src/json_parser/mod.rs
@@ -14,6 +14,28 @@ pub fn get_exec_path(file_path: &str) -> Result<path::PathBuf, io::Error> {
     Ok(json_path)
 }
 
+fn init_json_file(
+    json_path: &path::Path,
+    template_name: &str,
+    default_repo: &str,
+) -> Result<models::ConfigModel, io::Error> {
+    let mut config_model = models::ConfigModel::new(template_name.to_string(), true, default_repo);
+    config_model.first_init(template_name, default_repo)?;
+
+    let mut json_file = fs::File::create(json_path)?;
+    let serialize = serde_json::to_string_pretty(&config_model)?;
+
+    match json_file.write_all(serialize.as_bytes()) {
+        Ok(_) => {}
+        Err(_) => {
+            let mut file = fs::File::create(json_path)?;
+            file.write_all(serialize.as_bytes())?;
+        }
+    };
+
+    Ok(config_model)
+}
+
 pub fn parse_json_file(
     file_path: &str,
     template_name: &str,
@@ -24,23 +46,17 @@ pub fn parse_json_file(
     json_path.push(file_path);
 
     dbg!("Reading json file from: ", json_path.to_str());
-    let json_file = fs::File::open(&json_path).expect("Failed");
+
+    if fs::metadata(&json_path).is_err() {
+        dbg!("Json file not found, creating new one");
+        let config_model = init_json_file(&json_path, template_name, default_repo)?;
+        return Ok(config_model);
+    }
+
+    let json_file = fs::File::open(&json_path)?;
     let reader = io::BufReader::new(json_file);
 
     let mut config_model: models::ConfigModel = serde_json::from_reader(reader)?;
-
-    if !config_model.first_run {
-        let mut json_file = fs::File::open(&json_path)?;
-        config_model.first_init(template_name, default_repo)?;
-        let serialize = serde_json::to_string(&config_model)?;
-        match json_file.write_all(serialize.as_bytes()) {
-            Ok(_) => {}
-            Err(_) => {
-                let mut file = fs::File::create(&json_path)?;
-                file.write_all(serialize.as_bytes())?;
-            }
-        };
-    }
 
     Ok(config_model)
 }
